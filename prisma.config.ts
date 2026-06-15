8086726dae79a53d2382d3a3b370b0abe8d755df
Odinn Thorhallsson <odinn@Odinns-MacBook-Air-2.local>
2026-06-15 21:05:23 +0200

    fix prisma datasource

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,23 +1,11 @@
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-
   schema: "prisma/schema.prisma",
-
   migrations: {
-
     path: "prisma/migrations",
-
   },
-
   datasource: {
-
-    db: {
-
-      url: "postgresql://postgres:neqmo7-nykxys-negqyH@db.vyrwfjdmqadfrpddjslj.supabase.co:5432/postgres",
-
-    },
-
+    url: "postgresql://postgres:neqmo7-nykxys-negqyH@db.vyrwfjdmqadfrpddjslj.supabase.co:5432/postgres",
   },
-
 });
